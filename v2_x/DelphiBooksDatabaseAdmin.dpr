6d527ad155588626a45036bb2b1b8a9c5c019f18
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-14 09:57:31 +0200

    updated paths of used units from libraries and aboutbox

--- v2_x/DelphiBooksDatabaseAdmin.dpr
+++ v2_x/DelphiBooksDatabaseAdmin.dpr
@@ -4,11 +4,8 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   fMain in 'fMain.pas' {frmMain},
-  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
-  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
   DelphiBooks.Classes in '..\lib-externes\DelphiBooks-Common\src\DelphiBooks.Classes.pas',
   DelphiBooks.DB.Repository in '..\lib-externes\DelphiBooks-Common\src\DelphiBooks.DB.Repository.pas',
-  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
   uDMAboutBoxImage in 'uDMAboutBoxImage.pas' {DMAboutBoxImage: TDataModule},
   fBooks in 'fBooks.pas' {frmBooks},
   DelphiBooks.Tools in '..\lib-externes\DelphiBooks-Common\src\DelphiBooks.Tools.pas',
@@ -20,7 +17,10 @@ uses
   fBooksPublishers in 'fBooksPublishers.pas' {frmBooksPublishers},
   fBooksAuthors in 'fBooksAuthors.pas' {frmBooksAuthors},
   fBookCoverImage in 'fBookCoverImage.pas' {frmBookCoverImage},
-  fDescriptions in 'fDescriptions.pas' {frmDescriptions};
+  fDescriptions in 'fDescriptions.pas' {frmDescriptions},
+  u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas',
+  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialog.pas',
+  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\src\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm};
 
 {$R *.res}
 
